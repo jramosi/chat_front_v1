ba874a4910bdf5d7984b8c17a6da6ec8278266d1
jramosi <jkevinramos59@gmail.com>
2024-03-04 18:57:58 -0400

    proyecto primera version

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import "./App.css";
+import io from "socket.io-client";
+import { Chat } from "./Chat";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Box,
+  Divider,
+  Typography,
+  TextField,
+  Button,
+  Stack,
+  Container,
+} from "@mui/material";
+
+const socket = io.connect("http://localhost:3001");
+function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if (username != "" && room != "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  };
+
+  return (
+    <Container>
+      {!showChat ? (
+        <Card sx={{ width: "100%" }}>
+          <CardHeader title="Unirme al Chat" align="left" />
+          <Divider />
+          <CardContent>
+            <Box>
+              <Stack spacing={2}>
+                <TextField
+                  fullWidth
+                  name="username"
+                  label="Nombre de Usuario"
+                  type="text"
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+                <TextField
+                  fullWidth
+                  name="room"
+                  type="text"
+                  label="Sala"
+                  onChange={(e) => setRoom(e.target.value)}
+                />
+                <Button
+                  onClick={joinRoom}
+                  variant="contained"
+                  color="primary"
+                  sx={{ width: "300px" }}
+                >
+                  Unirse a la conversacion
+                </Button>
+              </Stack>
+            </Box>
+          </CardContent>
+
+          <Box sx={{ p: 2 }}>
+            <Typography color="text.secondary" variant="body2">
+              Pinstriped cornflower blue cotton blosaaaaaause takes you on a
+              walk to the park or just down the hall.
+            </Typography>
+          </Box>
+        </Card>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/Chat.jsx
@@ -0,0 +1,129 @@
+import {
+  Box,
+  Card,
+  CardHeader,
+  CardContent,
+  Divider,
+  TextField,
+  Button,
+  Paper,
+  Typography,
+  Tabs,
+} from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import { useEffect, useState } from "react";
+
+export const Chat = ({ socket, username, room }) => {
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messagesList, setMessagesList] = useState([]);
+  const sendMessage = async () => {
+    if (username && currentMessage) {
+      const info = {
+        message: currentMessage,
+        room,
+        author: username,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+      await socket.emit("send_message", info);
+      setMessagesList((list) => [...list, info]);
+
+      setCurrentMessage("");
+    }
+  };
+
+  useEffect(() => {
+    const messaHandle = (data) => {
+      setMessagesList((list) => [...list, data]);
+    };
+    socket.on("receive_message", messaHandle);
+    return () => socket.off("receive_message", messaHandle);
+  }, [socket]);
+
+  const presionadoEnter = (e) => {
+    if (e.key === "Enter") {
+      console.log("presionado");
+    }
+  };
+
+  return (
+    <Box display={"flex"}>
+      <Card sx={{ width: "100%" }}>
+        <CardHeader title={`Chat en Vivo  |   Sala: ${room}`} />
+
+        <Divider />
+        <CardContent variant="tabs">
+          {messagesList.map((item, i) => {
+            return (
+              <Box
+                variant="div"
+                key={i}
+                display={"flex"}
+                justifyContent={username === item.author ? "right" : "left"}
+              >
+                <Paper
+                  elevation={4}
+                  sx={{
+                    margin: 2,
+                    padding: 2,
+                    maxWidth: "50%",
+                    width: "fit-content",
+                    bgcolor: "#DCF8C6",
+                  }}
+                >
+                  <Box sx={{}}>
+                    <Typography variant="h6" fontWeight={"bold"}>
+                      {item.author}
+                    </Typography>
+
+                    <Typography variant="p">{item.message}</Typography>
+
+                    <Typography>{item.time}</Typography>
+                  </Box>
+                </Paper>
+              </Box>
+            );
+          })}
+        </CardContent>
+        <Paper
+          sx={{
+            mt: "20px",
+            p: "2px 4px",
+            display: "flex",
+            alignItems: "center",
+            width: "100%",
+          }}
+        >
+          <TextField
+            fullWidth
+            type="text-area"
+            label="Mensaje..."
+            size="small"
+            onChange={(e) => {
+              setCurrentMessage(e.target.value);
+            }}
+            sx={{
+              maxWidth: "100%",
+            }}
+            value={currentMessage}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                sendMessage();
+              }
+            }}
+          />
+          <Button
+            variant="contained"
+            sx={{ m: "8px" }}
+            endIcon={<SendIcon />}
+            onClick={sendMessage}
+          >
+            Enviar
+          </Button>
+        </Paper>
+      </Card>
+    </Box>
+  );
+};
