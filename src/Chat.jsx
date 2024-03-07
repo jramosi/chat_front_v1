08f85c92022f75b977d35cfc7ba6a9d0a28b43c3
jramosi <jkevinramos59@gmail.com>
2024-03-07 15:49:04 -0400

    cambios estetica

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -49,7 +49,7 @@ export const Chat = ({ socket, username, room }) => {
 
       <Divider />
       <ScrollToBottom>
-        <CardContent variant="tabs" sx={{ height: 400 }}>
+        <CardContent variant="tabs" sx={{ height: 350 }}>
           {messagesList.map((item, i) => {
             const bgcolor = username === item.author ? "#DCF8C6" : " #a2d9ce  ";
             return (
